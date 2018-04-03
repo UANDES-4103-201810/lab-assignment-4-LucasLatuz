601673a0fee427da34865f7692402fbfa508fd85
CaroAssadi <csassadi@miuandes.cl>
2018-04-03 12:24:11 -0300

    User and Place are ready

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,6 @@
 class Place < ApplicationRecord
 	has_many :events
+	validates :name, length: { minimum: 20 }
+	validates_numericality_of :capacity
+	validates :capacity, numericality: {greater_than: 0}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   	has_many :user_tickets	
 	has_many :tickets, :through => :user_tickets
+	validates :phone, length: {in: 9..12}
+	validates :password, with: /^[A-Za-z0-9]+$/
+	validates :password, length: {in: 8..12}
+	validates :email, format: {with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/}
+
 end
